d20a8ccfd5eaa0ec956d2fde4e07c49917bccc62
Tadgh Wagstaff <tadgh@tadghwagstaff.com>
2026-03-14 15:31:18 +0100

    figuring out sleeping container

--- rebuild-workspace-container.sh
+++ rebuild-workspace-container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 sudo podman container rm workspace-container
 sudo podman build . --tag=workspace-image
+sudo podman run --name workspace-container --volume ~/projects:/home/tadgh/projects --volume ~/.ssh/id_ed25519:/home/tadgh/.ssh/id_ed25519:ro --volume ~/.ssh/id_ed25519.pub:/home/tadgh/.ssh/id_25519.pub:ro --publish 127.0.0.1:2222:22/tcp -it workspace-image
